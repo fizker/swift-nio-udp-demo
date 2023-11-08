8d1fbf37ba8fee53b9da10e5ed1430e6d5d87fd1
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-11-08 10:00:00 +0100

    Rewrote echo server to print at various moments for debug purposes

--- Sources/AsyncEchoServer/main.swift
+++ Sources/AsyncEchoServer/main.swift
@@ -1,10 +1,14 @@
 import Foundation
 
-if ProcessInfo.processInfo.arguments.contains("ping") {
-	let server = try await EchoServer(port: 9091)
+let isPing = ProcessInfo.processInfo.arguments.contains("ping")
+let port = isPing ? 9091 : 9090
+
+let server = try await EchoServer(port: port)
+print("Server is up")
+
+if isPing {
+	print("Sending ping")
 	try await server.send(to: "127.0.0.1", port: 9090)
-	try await server.waitUntilClose()
-} else {
-	let server = try await EchoServer(port: 9090)
-	try await server.waitUntilClose()
 }
+
+try await server.waitUntilClose()
